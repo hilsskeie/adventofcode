5347181c2e3e3563d3582bec86e1162e14e30445
Hilde Solesvik Skeie <hss@usit.uio.no>
2018-12-02 12:10:39 +0100

    Done with day 1 part two

--- 2018/day1.py
+++ 2018/day1.py
@@ -1,15 +1,39 @@
 from pylab import *
 
-lines = open('day1_input.txt').readlines()
+Lines = open('day1_input.txt').readlines()
 
-data = []
-FinalSum = 0
+Data = []
 
-for elements in lines:
-	line = elements.strip('\n')
-	data.append(int(line))
+for Elements in Lines:
+	Line = Elements.strip('\n')
+	Data.append(int(Line))
 
-for element in data:
-	FinalSum+=element
+FinalSum = sum(Data)
+
 
 print FinalSum
+
+#Part 2
+
+Inlist = False
+Count = 0
+Frequency = [0]
+
+def CheckFrequency(Frequency,Data, Inlist, Count):
+	for i in range (len(Data)):
+		NewFrequency = Data[i]+Frequency[-1]
+		if NewFrequency in Frequency:
+			print("Frequency = %.f" % NewFrequency)
+			Inlist = True
+			break
+		elif i == (len(Data)-1):
+			Frequency.append(NewFrequency)
+			Count += 1
+			print('Count = %.f' % Count)
+		else:
+			 Frequency.append(NewFrequency)
+	return Inlist, Count
+
+while Inlist == False:
+	Inlist, Count = CheckFrequency(Frequency ,Data, Inlist, Count)
+
