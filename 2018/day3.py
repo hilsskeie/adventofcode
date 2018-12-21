cb49e4ab16e2d45f506cdb013cc641ab9fa3abf5
Hilde Solesvik Skeie <hss@usit.uio.no>
2018-12-21 12:17:38 +0100

    Done with day 3, part one

---
+++ 2018/day3.py
@@ -0,0 +1,50 @@
+from pylab import *
+
+Lines = open('day3_input.txt').readlines()
+
+Data = []
+
+for Elements in Lines:
+	Element = Elements.replace('\n', '')
+	Data.append(Element.split(' '))
+
+XStart = []
+YStart = []
+XSize = []
+YSize = []
+XSqaure = []
+YSquare = []
+
+for i in range(len(Data)):
+	start = Data[i][2].replace(':','').split(',')
+	size = Data[i][3].split('x')
+	line = Data[i][0].replace('#','')
+	
+
+	XStart.append(int(start[0]))
+	YStart.append(int(start[1]))
+	XSize.append(int(size[0]))
+	YSize.append(int(size[1]))
+	
+	squarex = int(start[0]) + int(size[0])
+	squarey = int(start[1]) + int(size[1])
+	XSqaure.append(squarex)
+	YSquare.append(squarey)
+
+MaxX = max(XSqaure)
+MaxY = max(YSquare)
+
+Matrix = zeros((MaxX,MaxY))
+
+for i in range(len(XStart)):
+	Matrix[XStart[i]:(XStart[i]+XSize[i]),YStart[i]:(YStart[i]+YSize[i])]+=1
+	
+Count = 0
+
+for Lines in Matrix:
+	for Elements in Lines:
+		if Elements >= 2:
+			Count +=1
+		else:
+			pass
+print Count
